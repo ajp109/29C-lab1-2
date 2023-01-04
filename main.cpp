c2e834bba4f0715fafea3b1057d910fb80b922de
ajp109 <andrew.pomfret@york.ac.uk>
2023-01-04 16:11:09 +0000

    Initial commit of project-specific code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include "mbed.h"
 
 int main() {
+
+    int number = 10;
+
+    int *pointer = &number;
+
+    printf("Number is %d, stored at address %p\n", number, &number);
+    printf("Pointer is %p, target value is %d\n", pointer, *pointer);
+
+    *pointer = 12;
+
+    printf("Number is %d, stored at address %p\n", number, &number);
+    printf("Pointer is %p, target value is %d\n", pointer, *pointer);
+
     while (true) {
         sleep();
     }
